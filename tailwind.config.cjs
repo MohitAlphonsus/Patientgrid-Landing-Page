55bbee94407623318a222ecc38b2f2077d3e34dc
MohitAlphonsus <mohitdalphonso@gmail.com>
2023-02-13 00:34:16 +0530

    initial setup with tailwind and sass using vite

---
+++ tailwind.config.cjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+	content: ['./src/**/*.{html,js}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				poppins: ['Poppins', 'sans-serif'],
+			},
+			colors: {
+				'color-header': '#E8E8E8',
+				'color-background': '#F8F8F8',
+				'color-primary': '#2A0896',
+				'color-secondary': '#87CEFA',
+				'color-tertiary': '#4482C2',
+				'color-neutral-1': '#DDE9FF',
+				'color-neutral-2': '#D9DEEF',
+			},
+		},
+	},
+	plugins: [],
+};
